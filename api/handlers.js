fcd00004bb92c7132bf7da66f670f996e9863364
Pedro Novoa <pedronovoa.21@gmail.com>
2023-04-26 19:04:48 -0300

    feat: added new user function and endpoint

--- api/handlers.js
+++ api/handlers.js
@@ -1,3 +1,8 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const tableName = 'stone-users';
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 const countapi = require('countapi-js');
 
 function extractBody(event) {
@@ -11,11 +16,34 @@ function extractBody(event) {
   return JSON.parse(event.body);
 }
 
+function generateToken(user) {
+  const payload = {
+    id: user.id,
+    name: user.name,
+    password: user.password,
+  };
+  return jwt.sign(payload, secret);
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, secret);
+}
+
 module.exports.showTonVisitCount = async (event) => {
 
-  let result = await countapi.get('ton.com.br', '7c84145f-8ffc-4873-b6f7-3e92214f4267')
+  const token = event.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'No token provided' });
 
-  console.log(result)
+  let result;
+
+  try {
+    verifyToken(token);
+    result = await countapi.get('ton.com.br', '7c84145f-8ffc-4873-b6f7-3e92214f4267')
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({ message: 'Invalid token' });
+  }
 
   return {
     statusCode: 200,
@@ -25,12 +53,54 @@ module.exports.showTonVisitCount = async (event) => {
 
 module.exports.incrementTonVisitCount = async (event) => {
 
-  let result = await countapi.hit('ton.com.br', '7c84145f-8ffc-4873-b6f7-3e92214f4267')
+  const token = event.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'No token provided' });
 
-  console.log(result)
+  let result;
+
+  try {
+    verifyToken(token);
+    result = await countapi.hit('ton.com.br', '7c84145f-8ffc-4873-b6f7-3e92214f4267')
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({ message: 'Invalid token' })
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify(result),
+  }
+};
+
+module.exports.addNewUser = async (event) => {
+
+  const { id, name, password } = extractBody(event);
+
+  const user = {
+    id: id,
+    name: name,
+    password: password
+  };
+
+  const params = {
+    TableName: tableName,
+    Item: user
+  };
+
+  const token = generateToken(user);
+
+  try {
+    await dynamodb.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `User created successfully with token: ${token}` })
+    };
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Unable to create user' })
+    };
   }
 };
